1602b3011ee80171dd28d1fe49cbbb00a11c422e
franzeneduardo <dudaccf@gmail.com>
2023-05-21 11:01:48 -0300

    centralized container

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import Input from '../Input';
+
+const ContentContainer = styled.div`
+  margin: 5% 10%;
+`;
 
 function Content() {
   return (
-    <div>
+    <ContentContainer>
       <Input label="Buscar" type="text" placeholder="Enter your search query" />
-    </div>
+    </ContentContainer>
   );
 }
 
